8b0bb952ff6b55bf9a612f60a40d79af8cf32e15
SAJNUS <sajnussaharearhojayfa@gmail.com>
2026-03-30 20:43:06 +0600

    switch to createHashRouter for github pages compatibility

--- src/router.jsx
+++ src/router.jsx
@@ -1,8 +1,8 @@
-import { createBrowserRouter } from 'react-router-dom'
+import { createHashRouter } from 'react-router-dom'
 import MainLayout from './layouts/MainLayout'
 import Home from './pages/Home'
 
-const router = createBrowserRouter([
+const router = createHashRouter([
   {
     path: '/',
     element: <MainLayout />,
